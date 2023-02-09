15cd4bcf548951d34a9143105ab3b04a76d271db
Rick Flagg <richard.lee.dlagg@outlook.com>
2023-02-09 08:41:05 -0500

    Adding core data files

--- servers/fast-api/main.py
+++ servers/fast-api/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise
+from database.register import register_tortoise
+from database.config import TORTOISE_ORM
 
 app = FastAPI()
+
+# enable schemas to read relationship between models
+Tortoise.init_models(["database.models"], "models")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")

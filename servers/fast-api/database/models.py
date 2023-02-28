ed4ec4024e0f226f670d27e032bcf2385f7e36e3
Rick Flagg <richard.lee.flagg@outlook.com>
2023-02-27 20:51:31 -0500

    Refactoring project data access

--- servers/fast-api/database/models.py
+++ servers/fast-api/database/models.py
@@ -1,5 +1,6 @@
 from tortoise import fields, models
 
+
 class Users(models.Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=20, unique=True)
@@ -8,6 +9,17 @@ class Users(models.Model):
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
 
+
 class Incident(models.Model):
     id = fields.IntField(pk=True)
     external_case_number = fields.CharField(max_length=20, unique=True)
+    ip = fields.CharField(max_length=30)
+    pen_tester = fields.BooleanField()
+    region = fields.CharField(max_length=50)
+    country = fields.CharField(max_length=50)
+    script_injection = fields.BooleanField()
+    command_injection = fields.BooleanField()
+    sql_injection = fields.BooleanField()
+    cross_frame_scripting = fields.BooleanField()
+    path_discovery = fields.BooleanField()
+    other = fields.BooleanField()
